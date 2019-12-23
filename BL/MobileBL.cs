7a44cea7a53f0e5504ddd57abd2e6a25d0ed4d6e
kitaro95 <cuonglm.nde19002@vtc.edu.vn>
2019-12-23 17:05:21 +0700

    third commit

---
+++ BL/MobileBL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Persistion;
+using DAL;
+using System.Collections.Generic;
+
+namespace BL
+{
+    public class MobileBL
+    {
+        private MobileDAL mobileDAL;
+
+        public MobileBL()
+        {
+            mobileDAL = new MobileDAL();
+        }
+        public Mobile GetMobilebyId(int MobileID)
+        { 
+           return mobileDAL.GetMobilebyId(MobileID);
+        }
+        public List<Mobile> GetListMobile()
+        {
+            return mobileDAL.GetListMobile();
+        }
+    }
+}
---
+++ Persistion/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Persistion
+{
+    public class Customer
+    {
+        public int CustomerID {get;set;}
+        public string CustomerUsername {get;set;}
+        public string CustomerPhonenumber {get;set;}
+        public string CustomerEmail {get;set;}
+        public string CustomerAddress{get;set;}
+        public DateTime CustomerBirthday {get;set;}
+        public string CustomerName {get;set;}
+        public string CustomerGender {get;set;}
+    }
+}
